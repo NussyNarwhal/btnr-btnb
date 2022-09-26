a9260df3b30ffcfd451d6a3185d98b199ae0ee9a
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-09-26 12:19:15 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,24 @@
-let strip = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
-strip.setBrightness(255)
-let btn1B = pins.digitalReadPin(DigitalPin.P16)
-let btn2B = pins.digitalReadPin(DigitalPin.P16)
-let btn1R = pins.digitalReadPin(DigitalPin.P8)
-let btn2R = pins.digitalReadPin(DigitalPin.P8)
+basic.showIcon(IconNames.Yes)
+let btn1B = pins.digitalReadPin(DigitalPin.P5)
+let btn2B = pins.digitalReadPin(DigitalPin.P5)
+let btn1R = pins.digitalReadPin(DigitalPin.P11)
+let btn2R = pins.digitalReadPin(DigitalPin.P11)
+pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
 basic.forever(function () {
-    for (let index = 0; index <= 47; index++) {
-        strip.setPixelColor(index, neopixel.rgb(randint(8, 47), randint(0, 47), randint(8, 47)))
-        strip.show()
+    if (pins.digitalReadPin(DigitalPin.P5) == 0) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.clearScreen()
     }
 })
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2)) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        for (let index = 0; index < 60; index++) {
-            strip.showRainbow(1, 360)
-            strip.show()
-        }
+    if (pins.digitalReadPin(DigitalPin.P11) == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.clearScreen()
     }
 })
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2)) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        for (let index = 0; index < 60; index++) {
-            strip.showRainbow(1, 360)
-            strip.show()
-        }
-    }
+	
 })
